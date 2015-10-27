ebecc4a8755fe614d7431680c919971c1d5c5c53
Luke Richards <luke.richards@emailcenteruk.com>
2015-10-27 10:12:55 +0000

    Switch to using Phlib Console
    Changed to using Phlib console for the configuration helper. Tidied up
    some of the code.

--- src/Console/AbstractCommand.php
+++ src/Console/AbstractCommand.php
@@ -3,8 +3,6 @@
 namespace Phlib\Beanstalk\Console;
 
 use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Output\OutputInterface;
 use Phlib\Beanstalk\Factory;
 
 /**
@@ -13,21 +11,10 @@ use Phlib\Beanstalk\Factory;
  */
 abstract class AbstractCommand extends Command
 {
-    use ConfigurableCommandTrait;
-
     /**
      * @var \Phlib\Beanstalk\BeanstalkInterface
      */
     protected $beanstalk;
-
-    /**
-     * @inheritdoc
-     */
-    protected function initialize(InputInterface $input, OutputInterface $output)
-    {
-        parent::initialize($input, $output);
-        $this->loadConfiguration($input);
-    }
 
     /**
      * @return \Phlib\Beanstalk\BeanstalkInterface
@@ -36,7 +23,7 @@ abstract class AbstractCommand extends Command
     {
         if (!$this->beanstalk) {
             $factory = new Factory;
-            $config = $this->getConfiguration();
+            $config = $this->getHelper('configuration')->fetch();
             if ($config === false) {
                 $this->beanstalk = $factory->create('localhost');
             } else {
