8382feb689e48621338c5aa60b748027bbd4b6df
Luke Richards <luke.richards@emailcenteruk.com>
2015-11-03 10:16:04 +0000

    Implemented round robin strategy for pool
    Implemented the class and associated tests.

--- src/Pool/RoundRobinStrategy.php
+++ src/Pool/RoundRobinStrategy.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace Phlib\Beanstalk\Pool;
+use Phlib\Beanstalk\Exception\InvalidArgumentException;
 
 /**
  * Class RoundRobinStrategy
@@ -8,8 +9,53 @@ namespace Phlib\Beanstalk\Pool;
  */
 class RoundRobinStrategy implements SelectionStrategyInterface
 {
+    /**
+     * @var string[]
+     */
+    protected $collection = null;
+
+    /**
+     * @var string
+     */
+    protected $check = '';
+
+    /**
+     * @var integer
+     */
     protected $index = null;
-    public function pickOne($collection)
+
+    /**
+     * @inheritdoc
+     */
+    public function pickOne(array $collection)
     {
+        if (empty($collection)) {
+            throw new InvalidArgumentException('Can not select from an empty collection.');
+        }
+        $this->setup($collection);
+
+        $index = $this->index;
+        $this->index++;
+        if ($this->index > (count($this->collection) - 1)) {
+            $this->index = 0;
+        }
+
+        return $this->collection[$index];
+    }
+
+    /**
+     * @param string[] $collection
+     */
+    protected function setup(array $collection)
+    {
+        if (!is_null($this->collection) && serialize($collection) == $this->check) {
+            return;
+        }
+
+        $this->collection = $collection;
+        $this->check      = serialize($collection);
+        if (is_null($this->index) || $this->index > (count($this->collection) - 1)) {
+            $this->index = 0;
+        }
     }
 }
---
+++ tests/Pool/RoundRobinStrategyTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Phlib\Tests\Pool;
+
+use Phlib\Beanstalk\Pool\RoundRobinStrategy;
+
+class RoundRobinStrategyTest extends \PHPUnit_Framework_TestCase
+{
+    public function testImplementsSelectionStrategyInterface()
+    {
+        $this->assertInstanceOf('\Phlib\Beanstalk\Pool\SelectionStrategyInterface', new RoundRobinStrategy());
+    }
+
+    public function testAllowsContinuousSelection()
+    {
+        $firstHost = 'host123';
+        $keys = [$firstHost, 'host456', 'host789'];
+        $strategy = new RoundRobinStrategy();
+        for ($i = 0; $i < count($keys); $i++) {
+            $strategy->pickOne($keys);
+        }
+        $this->assertEquals($firstHost, $strategy->pickOne($keys));
+    }
+
+    /**
+     * @expectedException \Phlib\Beanstalk\Exception\InvalidArgumentException
+     */
+    public function testFailsWhenGivenEmptyCollection()
+    {
+        (new RoundRobinStrategy())->pickOne([]);
+    }
+
+    public function testWithDifferingCollections()
+    {
+        $strategy = new RoundRobinStrategy();
+
+        $keys = ['host123'];
+        $strategy->pickOne($keys);
+
+        $newHost = 'host456';
+        $keys[] = $newHost;
+        $this->assertEquals($newHost, $strategy->pickOne($keys));
+    }
+}
