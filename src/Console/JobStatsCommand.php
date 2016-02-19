22389970d4fe6edecba1034f3cfc9a29f1c94b99
Luke Richards <luke.richards@emailcenteruk.com>
2016-02-19 11:37:47 +0000

    Improve output and augment job stats
    Removed the var_export that wasn’t very pretty. Pulled actual stats for
    the job and displayed in a  table. Calculated specific date times for
    age and delay.

--- src/Console/JobStatsCommand.php
+++ src/Console/JobStatsCommand.php
@@ -3,6 +3,7 @@
 namespace Phlib\Beanstalk\Console;
 
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Helper\Table;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
@@ -21,7 +22,34 @@ class JobStatsCommand extends AbstractCommand
     {
         $jobId = $input->getArgument('job-id');
         $stats = $this->getBeanstalk()->statsJob($jobId);
-        $output->writeln("Found Job '$jobId'.");
-        $output->writeln(var_export($stats, true)); // TODO: this needs to be prettier?
+        $output->writeln("<info>[Job ID: $jobId]</info>");
+
+        $table = new Table($output);
+        $table->setStyle('compact');
+        $details = array_intersect_key($stats, array_flip(['tube', 'state']));
+        foreach ($details as $detail => $value) {
+            if ($detail == 'state' && $value == 'buried') {
+                $value = "<error>{$value}</error>";
+            }
+            $table->addRow([$detail, $value]);
+        }
+        $table->render();
+
+        $created = time();
+        $table   = new Table($output);
+        $stats   = array_diff_key($stats, array_flip(['id', 'tube', 'state']));
+        $table->setHeaders(['Stat', 'Value']);
+        foreach ($stats as $stat => $value) {
+            if ($stat == 'age') {
+                $created = time() - (int)$value;
+                $dt = date('Y-m-d H:i:s', $created);
+                $table->addRow(['created', $dt]);
+            } elseif ($stat == 'delay') {
+                $dt = date('Y-m-d H:i:s', $created + (int)$value);
+                $table->addRow(['scheduled', $dt]);
+            }
+            $table->addRow([$stat, $value]);
+        }
+        $table->render();
     }
 }
