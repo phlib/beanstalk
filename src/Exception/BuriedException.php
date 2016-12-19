a479bd2bb0ed668244d5f0f6e1e49ca18a7dd5c8
Luke Richards <luke.richards@emailcenteruk.com>
2016-12-19 13:03:38 +0000

    Add exception types for buried job and draining server

---
+++ src/Exception/BuriedException.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Phlib\Beanstalk\Exception;
+
+class BuriedException extends CommandException
+{
+    public static function create(int $jobId): self
+    {
+        return new static($jobId, 'Server ran out of memory trying to grow the priority queue data structure');
+    }
+
+    private int $jobId;
+
+    public function __construct(int $jobId, string $message, int $code = 0, \Exception $previous = null)
+    {
+        $this->jobId = $jobId;
+        parent::__construct($message, $code, $previous);
+    }
+
+    public function getJobId(): int
+    {
+        return $this->jobId;
+    }
+}
