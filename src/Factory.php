1bb057fc3a30f305c6777e99d5e9d1d618ea992c
Luke Richards <luke.richards@emailcenteruk.com>
2016-12-20 12:28:15 +0000

    Removed all strategy classes

--- src/Factory.php
+++ src/Factory.php
@@ -7,7 +7,6 @@ use Phlib\Beanstalk\Connection\ConnectionInterface;
 use Phlib\Beanstalk\Exception\InvalidArgumentException;
 use Phlib\Beanstalk\Connection\Socket;
 use Phlib\Beanstalk\Pool\Collection;
-use Phlib\Beanstalk\Pool\SelectionStrategyInterface;
 
 class Factory
 {
