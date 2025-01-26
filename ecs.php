97565e47371c7191c833e45101624d157931f411
Chris Minett <1084019+chrisminett@users.noreply.github.com>
2025-01-26 09:15:50 +0000

    Update ECS

--- ecs.php
+++ ecs.php
@@ -7,11 +7,10 @@ use Symplify\EasyCodingStandard\ValueObject\Set\SetList;
 
 return ECSConfig::configure()
     ->withPaths([
-        __DIR__ . '/bin',
-        __DIR__ . '/bin/beanstalk',
         __DIR__ . '/src',
         __DIR__ . '/tests',
     ])
+    ->withRootFiles()
 
     ->withSets([
         SetList::COMMON,
@@ -26,18 +25,30 @@ return ECSConfig::configure()
         // Remove sniff, from common/control-structures
         \PhpCsFixer\Fixer\ClassNotation\OrderedClassElementsFixer::class,
 
-        // Remove sniff, from common/docblock, due to documented params for `Collection::sendToAll()` callables
-        \PhpCsFixer\Fixer\Phpdoc\NoSuperfluousPhpdocTagsFixer::class,
-
         // Remove sniff, from common/spaces
         \PhpCsFixer\Fixer\Operator\NotOperatorWithSuccessorSpaceFixer::class,
-        \PhpCsFixer\Fixer\CastNotation\CastSpacesFixer::class,
     ])
 
-    // PER Coding Style 7.1: "The `fn` keyword MUST NOT be succeeded by a space."
+    // Rule from common/spaces. No space after cast.
+    ->withConfiguredRule(
+        \PhpCsFixer\Fixer\CastNotation\CastSpacesFixer::class,
+        [
+            'space' => 'none',
+        ],
+    )
+
+    /*
+     * Rule missing from PSR12. PER Coding Style 3:
+     * "... each of the blocks [of import statements] MUST be separated by a single blank line ..."
+     */
+    ->withRules([
+        \PhpCsFixer\Fixer\Whitespace\BlankLineBetweenImportGroupsFixer::class,
+    ])
+
+    // Rule from PSR12. PER Coding Style 7.1: "The `fn` keyword MUST NOT be succeeded by a space."
     ->withConfiguredRule(
         \PhpCsFixer\Fixer\FunctionNotation\FunctionDeclarationFixer::class,
         [
             'closure_fn_spacing' => 'none',
-        ]
+        ],
     );
